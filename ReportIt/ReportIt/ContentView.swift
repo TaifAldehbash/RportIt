e4ce82b2f8e3cde7336bd94c55dd669eb3fc3e38
“TaifAldehbashgit config --global user.email “taifmaldehbash@gmail.com <77803844+TaifAldehbash@users.noreply.github.com>
2024-04-04 06:30:58 +0300

    Project creation

---
+++ ReportIt/ReportIt/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ReportIt
+//
+//  Created by Taif Al.qahtani on 03/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReportIt/ReportIt/ReportItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReportItApp.swift
+//  ReportIt
+//
+//  Created by Taif Al.qahtani on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ReportItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
